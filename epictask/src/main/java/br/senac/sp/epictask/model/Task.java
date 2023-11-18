3937e65b7a554dd9affd8fdcdc781bac53a63e4a
LuckiM4n <142065275+LuckiM4n@users.noreply.github.com>
2023-11-17 21:54:30 -0300

    Main Atualizado
    Atualizado o projeto todo para incluir o método post

---
+++ epictask/src/main/java/br/senac/sp/epictask/model/Task.java
@@ -0,0 +1,31 @@
+package br.senac.sp.epictask.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Task {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    Long id;
+    @NotBlank @Size(min = 3, max = 50, message = "Digite entre 3 e 50 caracteres")
+    String title;
+
+    @Size(min= 10, max = 200, message="Digite uma descrição com um tamanho entre 10 e 200 caracteres")
+    String description;
+
+    @Min(value= 1, message = "Não pode ser negativo")
+    Integer score;
+
+}
